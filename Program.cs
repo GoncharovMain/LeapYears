103060ed81b25cda4f03c748727092830d2d69a2
GoncharovMain <goncharov.safe@gmail.com>
2021-12-07 14:08:43 +0300

    Clear Foo, Bar and Fizz.

--- Program.cs
+++ Program.cs
@@ -19,34 +19,9 @@
         }
         public static int GetLeapYearsRec(int start_year, int end_year, int count_leap_years = 0)
             => start_year <= end_year ? GetLeapYearsRec(start_year + 1, end_year, count_leap_years += (IsLeap(start_year) ? 1 : 0)) : count_leap_years;
-
-        public static void Foo(int a = 5) // необязательный параметр
-        {
-            Console.WriteLine(a);
-        }
-        public static void Bar()
-        {
-            int a = 5;
-            Console.WriteLine(a);
-        }
-        public static void Fizz()
-        {
-            Foo(6); // a => 6
-            Foo(); // a => 5
-            //Bar(6); // error - нельзя передать в метод
-        }
-        public static int Fact(int f)
-        {
-            if (f < 1)
-                return f;
-            return Fact(f - 1) * f;
-        }
         public static void Main()
         {
             Console.WriteLine("count leap years: -> " + GetLeapYearsRec(2000, 2030));
         }
     }
 }
-
-// [2000 2001 2002 2003 2004 2005 2006 2007 2008 2009 2010 2011 2012]
-
